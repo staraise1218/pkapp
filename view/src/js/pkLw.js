6f22da0630264d5f3fa59efd19888e7e77681e8b
guofeng-mi <guofeng_mi@sina.com>
2019-07-29 15:25:46 +0800

    akjdfnajnjkm

--- view/src/js/pkLw.js
+++ view/src/js/pkLw.js
@@ -1,9 +1,12 @@
-
-
+var GlobalHost = 'http://pkapp.staraise.com.cn'
+var gift_id = '';
+var to_user_id = '';
+var num = '1';
 
 
 $('#userList').delegate('.send','click', function () {
     console.log($(this).attr('data-id'))
+    to_user_id = $(this).attr('data-id');
     $(".lwWrap").show();
     $('.lw_bg').show();
 })
@@ -17,11 +20,56 @@ $('.lwWrap .close').click(function () {
 
 
 
+$.ajax({
+    type: 'post',
+    url: GlobalHost + '/Api/gift/myGift',
+    data: {
+        user_id: $user_id
+    },
+    success: function (res) {
+        console.log(res);
+
+        var list = '';
+        res.data.forEach(item => {
+            list += `<div class="item" data-id="${item.gift_id}">
+                        <img src="${GlobalHost + item.image}" alt="">
+                        <div class="t">
+                            <p>${item.price}</p>
+                            <img src="./src/img/1/jinbi.png" alt="">
+                        </div>
+                    </div>`
+            $('.lwWrap .con').html(list)
+        })
+
+
+    }
+})
 
 
 
-
-
+$('.lwWrap .con').delegate('.item', 'click', function () {
+    console.log($(this).attr('data-id'));
+    gift_id = $(this).attr('data-id');
+    $.ajax({
+        type: 'post',
+        url: GlobalHost + '/Api/gift/give',
+        data: {
+            user_id: $user_id,
+            to_user_id: to_user_id, 
+            gift_id: gift_id,
+            num: num
+        },
+        success: function (res) {
+            console.log(res)
+                $('.lwWrap').hide();
+                $('._lw_alert').text(res.msg).show();
+                setTimeout(() => {
+                    $('._lw_alert').hide();
+                    $('.lw_bg').hide();
+                }, 1000)
+        }
+    })
+})
 
 
 
